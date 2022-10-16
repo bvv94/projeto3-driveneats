fe0d0d3633ed880aa98d309d21221c49b6ec6236
bvv94 <bvdevasconcelos@gmail.com>
2022-10-15 23:40:45 -0400

    selecao sem eliminacao das outras opcoes

--- javascript.js
+++ javascript.js
@@ -1,3 +1,19 @@
-function selecionado(){
+let prato;
+let bebida;
+let sobremesa;
+
+function pratoselecionado(pratomarcado){
+    const sel = document.querySelector(".opcoes");
+    pratomarcado.classList.add ("bordaverde");
+    alert ('oi');
+}
+function bebidaselecionada(bebidamarcado){
+    const sel = document.querySelector(".opcoes");
+    bebidamarcado.classList.add ("bordaverde");
+    alert ('oi');
+}
+function sobremesaselecionada(sobremesamarcado){
+    const sel = document.querySelector(".opcoes");
+    sobremesamarcado.classList.add ("bordaverde");
     alert ('oi');
 }
