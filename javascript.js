283a1ac081faaeed2675a482a1e3903df3433cf5
bvv94 <bvdevasconcelos@gmail.com>
2022-10-16 10:11:57 -0400

    16out 10:11

--- javascript.js
+++ javascript.js
@@ -2,34 +2,36 @@ let prato;
 let bebida;
 let sobremesa;
 
-function pratoselecionado(pratomarcado){
+function pratoselecionado(pratomarcado) {
     const sel = document.querySelector(".opcoes");
-    pratomarcado.classList.add ("bordaverde");
+    pratomarcado.classList.add("bordaverde");
     const seta = document.querySelector(".ok");
     seta.classList.remove("ok");
     seta.classList.add("okgreen");
     prato = document.querySelector("nomeopcao");
     alert(prato);
-    
+
 }
-function bebidaselecionada(bebidamarcado){
+function bebidaselecionada(bebidamarcado) {
     const sel = document.querySelector(".opcoes");
-    bebidamarcado.classList.add ("bordaverde");
+    bebidamarcado.classList.add("bordaverde");
     const seta = document.querySelector(".ok");
     seta.classList.remove("ok");
     seta.classList.add("okgreen");
-    
+
 }
-function sobremesaselecionada(sobremesamarcado){
+function sobremesaselecionada(sobremesamarcado) {
     const sel = document.querySelector(".opcoes");
-    sobremesamarcado.classList.add ("bordaverde");
+    sobremesamarcado.classList.add("bordaverde");
     const seta = document.querySelector(".ok");
     seta.classList.remove("ok");
     seta.classList.add("okgreen");
-    
+
 }
-function finalizar(bot){
-    bot.classList.remove ("botao");
-    bot.classList.add ("botaook");
-    
+function finalizar(bot) {
+    if ((prato !== undefined) && (bebida !== undefined) && (sobremesa != undefined)) {
+        bot.innerHTML = "Fechar pedido"
+        bot.classList.remove("botao");
+        bot.classList.add("botaook");
+    }
 }
