d0a85b0ca4796f3fd8f2ee0e3231d5f843e6ba58
bvv94 <bvdevasconcelos@gmail.com>
2022-10-16 09:46:32 -0400

    markbuttonadded

--- javascript.js
+++ javascript.js
@@ -5,15 +5,31 @@ let sobremesa;
 function pratoselecionado(pratomarcado){
     const sel = document.querySelector(".opcoes");
     pratomarcado.classList.add ("bordaverde");
-    alert ('oi');
+    const seta = document.querySelector(".ok");
+    seta.classList.remove("ok");
+    seta.classList.add("okgreen");
+    prato = document.querySelector("nomeopcao");
+    alert(prato);
+    
 }
 function bebidaselecionada(bebidamarcado){
     const sel = document.querySelector(".opcoes");
     bebidamarcado.classList.add ("bordaverde");
-    alert ('oi');
+    const seta = document.querySelector(".ok");
+    seta.classList.remove("ok");
+    seta.classList.add("okgreen");
+    
 }
 function sobremesaselecionada(sobremesamarcado){
     const sel = document.querySelector(".opcoes");
     sobremesamarcado.classList.add ("bordaverde");
-    alert ('oi');
+    const seta = document.querySelector(".ok");
+    seta.classList.remove("ok");
+    seta.classList.add("okgreen");
+    
+}
+function finalizar(bot){
+    bot.classList.remove ("botao");
+    bot.classList.add ("botaook");
+    
 }
